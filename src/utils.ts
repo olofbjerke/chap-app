f374ef07476c4851ea1d01943f4c29dfaf7c8215
Olof Bjerke <olofbjerke@gmail.com>
2024-12-08 22:18:39 +0100

    refactor sessionSignal

--- src/utils.ts
+++ src/utils.ts
@@ -1,24 +1,20 @@
 import { effect, signal } from "@preact/signals";
 
 export function sessionSignal<T>(name: string, initialValue: T, initializer?: (deserializedData: any) => T) {
-    let s = createSessionSignal(name, initialValue, initializer);
+    let storedValue = sessionStorage.getItem(name);
+    if (storedValue) {
+        try {
+            initialValue = JSON.parse(storedValue);
+        } catch (e) {
+            initialValue = storedValue as any;
+        }
+    }
+
+    let s = signal(initializer ? initializer(initialValue) : initialValue);
+
     effect(() => {
         sessionStorage.setItem(name, JSON.stringify(s.value));
     });
 
     return s;
 }
-
-function createSessionSignal<T>(name: string, initialValue: T, initializer?: (deSerializedData: any) => T) {
-    let value = sessionStorage.getItem(name);
-    if (value) {
-        try {
-            initialValue = JSON.parse(value);
-        } catch (e) {
-            initialValue = value as any;
-        }
-    }
-
-    return signal(initializer ? initializer(initialValue) : initialValue);
-}
-
